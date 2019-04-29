eb098565ff60d1fca2af9b9b86ad849a7f42cac3
Evgeniy Gololobov <evgeniy.gololobov@gmail.com>
2019-04-29 18:10:05 +0300

    'iscan-new'

---
+++ app/Http/Controllers/OutsourceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Outsource;
+use Illuminate\Http\Request;
+
+class OutsourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Outsource  $outsource
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Outsource $outsource)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Outsource  $outsource
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Outsource $outsource)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Outsource  $outsource
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Outsource $outsource)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Outsource  $outsource
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Outsource $outsource)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_04_29_124638_create_orders_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('client_id')->nullable();
+            $table->unsignedInteger('status_id')->nullable();
+            $table->unsignedInteger('outsource_id')->nullable();
+            $table->unsignedInteger('service_id')->nullable();
+            $table->unsignedInteger('paper_id')->nullable();
+            $table->boolean('is_color')->default(0);
+            $table->boolean('is_non_color')->default(0);
+            $table->unsignedInteger('quantity')->nullable();
+            $table->tinyInteger('pay_type')->nullable();
+            $table->double('amount')->default(0);
+            $table->double('prepayment')->default(0);
+            $table->integer('price_design')->default(0);
+            $table->text('comment');
+            $table->boolean('is_files')->default(0);
+            $table->timestamp('date_end')->nullable();
+            $table->timestamps();
+
+            $table->index('client_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
