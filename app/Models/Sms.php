7ed424b23fc8e5f3a95bc62107698fabb6406332
multitoys <evgeniy.gololobov@gmail.com>
2020-03-29 18:39:37 +0300

    update sms status

--- app/Models/Sms.php
+++ app/Models/Sms.php
@@ -8,14 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Sms extends Model
 {
     const MAX_ATTEMPTS = 5;
-    
+
     const MESSAGE_UNKNOWN       = 0;
     const MESSAGE_IS_DELIVERED  = 1;
     const MESSAGE_IS_SENT       = 2;
     const MESSAGE_NOT_DELIVERED = 3;
     const MESSAGE_IN_QUEUE      = 4;
     const MESSAGE_NOT_EXIST     = 5;
-    
+
     const STATUSES = [
         'MESSAGE_UNKNOWN'       => 0,
         'MESSAGE_IS_DELIVERED'  => 1,
@@ -24,25 +24,31 @@ class Sms extends Model
         'MESSAGE_IN_QUEUE'      => 4,
         'MESSAGE_NOT_EXIST'     => 5,
     ];
-    
+
     protected $fillable = ['sms_id', 'order_id', 'type', 'message', 'is_sent', 'status', 'attempts'];
-    
+
     public function getSmsStatusAttribute()
     {
-        if ($this->status == self::MESSAGE_UNKNOWN && $this->sms_id) {
+        $statuses = [
+            self::MESSAGE_UNKNOWN,
+            self::MESSAGE_IS_SENT,
+            self::MESSAGE_IN_QUEUE,
+            self::MESSAGE_NOT_DELIVERED,
+        ];
+        if ($this->sms_id && in_array($this->status, $statuses)) {
             $this->status = SmsHelper::getStatus($this->sms_id);
             $this->save();
         }
-        
+
         return $this->status;
     }
-    
+
     public function getStatusTextAttribute()
     {
         if (!$this->is_sent) {
             return 'не отправлено';
         }
-        
+
         switch ($this->sms_status) {
             case self::MESSAGE_IS_DELIVERED:
                 $sms_status = 'доставлено';
@@ -64,10 +70,10 @@ class Sms extends Model
                 $sms_status = 'статус не известен';
                 break;
         }
-        
+
         return $sms_status;
     }
-    
+
     public function scopeSent($query)
     {
         return $query->where('sent', 1);
